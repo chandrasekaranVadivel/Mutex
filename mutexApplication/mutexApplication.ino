0db8df15851a6d3506ba0411f57986da5dcde15b
chandrasekaranVadivel <chasekan@gmail.com>
2023-06-14 09:27:01 +0530

    mutex not workig
    after the  structure is passed into the task then the task is not working . need to investigate

--- mutexApplication/mutexApplication.ino
+++ mutexApplication/mutexApplication.ino
@@ -1,20 +1,33 @@
+
+#include<stdlib.h>
+#include<string.h>
 #include <Arduino_FreeRTOS.h>
 #include <semphr.h>  // add the FreeRTOS functions for Semaphores (or Flags).
-#include<string.h>
-void task1(void *);
-void task2(void *);
+typedef struct data_h
+{
+  int num;
+  char name[20];
+}data_t;
+void task1(data_t *);
+void task2(data_t *);
 TaskHandle_t *task1_h,*task2_h;
 SemaphoreHandle_t mutex_h;
 void setup()
 {
   Serial.begin(9600);
-  xTaskCreate(task1, "task1", 500, "task1", 2, task1_h);
-  xTaskCreate(task1, "task2", 100, "task2", 2, task2_h);
+  data_t *data1=(data_t*)malloc(sizeof(data_t));
+  strcpy(data1->name,"task1");
+  data1->num=1000;
+  xTaskCreate(task1, "task1", 500, data1, 2, task1_h);
+  data_t *data2=(data_t*)malloc(sizeof(data_t));
+  strcpy(data2->name,"task2");
+  data2->num=1000;
+  xTaskCreate(task1, "task2", 500, &data2, 2, task2_h);
   mutex_h=xSemaphoreCreateMutex();
 }
 void loop()
 {}
-void task1(void * param)
+void task1(data_t *param)
 {
   char x[20];
   int y=0;
@@ -22,7 +35,7 @@ void task1(void * param)
   {
     if(xSemaphoreTake(mutex_h, pdMS_TO_TICKS(0))==pdPASS)
     {
-      sprintf(x,"%s,%d",(char*)param,y);
+      sprintf(x,"%s,%d",(char*)param->name,y);
       Serial.println(x);
       y++;
       xSemaphoreGive(mutex_h);
